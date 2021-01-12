ad9e780777563f2091a633f684c80e75a85c2403
github username <github email address>
2021-01-12 09:23:07 -0600

    Debounced

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,11 +6,21 @@ const Search = () => {
 
 
     const [term, setTerm] = useState('Search')
+    const [debouncedTerm, setDebouncedTerm] = useState(term)
     const [results, setResults] = useState([])
 
     
+// refactoring deals with bugs, and will NOT do another search if the same term is entered.
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term)
+        }, 1000)
 
-    
+        return () => {
+            clearTimeout(timerId)
+        }
+    }, [term])
+
     useEffect(() => {
         const search = async () => {
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -19,28 +29,14 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debouncedTerm
                 }
             })
             setResults(data.query.search)
         }
-        if(term && !results.length) {
-            search()
-        } else {
-            const timeoutId = setTimeout(() => {
-                if(term) {
-                search()
-            }
-    
-            }, 500)
-    
-            return () => {
-                clearTimeout(timeoutId)
-            }
+        search()
+    }, [debouncedTerm])
 
-
-        }
-    }, [term])
 
     const renderedResults = results.map((result) => {
         return <div key={result.pageid} className='item'>
